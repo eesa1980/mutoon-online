451f58d974f6e2d92cc5b9ac7711a8ff50524301
Lewis <lewis@webuildbots.ai>
2020-05-04 09:45:21 +0100

    added categories to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,27 @@
-import { Container, Typography } from "@material-ui/core";
+import { Container } from "@material-ui/core";
 import * as React from "react";
+import Categories from "../components/Categories";
 import DefaultLayout from "../layouts/DefaultLayout";
+import { AllWordpressCategory, Site } from "../model";
+import { AllWordpressWpMedia } from "../model/media";
 
 // Please note that you can use https://github.com/dotansimha/graphql-code-generator
 // to generate all types from graphQL schema
 interface IndexPageProps {
   data: {
-    site: {
-      siteMetadata: {
-        title: string;
-      };
-    };
+    allWordpressCategory: AllWordpressCategory;
+    allWordpressWpMedia: AllWordpressWpMedia;
+    site: Site;
   };
-  theme: any;
 }
 
 const Index: React.FC<IndexPageProps> = (props) => {
-  const [categoryVal, setCategoryVal] = React.useState<string>("");
-  const [bookVal, setBookVal] = React.useState<string>("");
+  const { site, ...categoriesData } = props.data;
 
   return (
     <DefaultLayout>
       <Container maxWidth="sm">
-        <Typography variant="h1" component="h1" align="center">
-          <strong>{props.data.site.siteMetadata.title}</strong>
-        </Typography>
+        <Categories data={{ ...categoriesData }} />
       </Container>
     </DefaultLayout>
   );
@@ -35,6 +32,34 @@ export const pageQuery = graphql`
     site {
       siteMetadata {
         title
+      }
+    }
+    allWordpressCategory {
+      edges {
+        node {
+          name
+          wordpress_parent
+          slug
+          wordpress_id
+        }
+      }
+    }
+    allWordpressWpMedia(filter: { media_type: { eq: "image" } }) {
+      nodes {
+        media_type
+        alt_text
+        title
+        categories {
+          name
+        }
+        localFile {
+          childImageSharp {
+            id
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
       }
     }
   }
